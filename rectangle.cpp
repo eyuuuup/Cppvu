2d857d06439a6ca93c4f15f16456ded10486085f
eyup <eyupvroegop@gmail.com>
2019-09-06 11:42:13 +0200

    added std to assignment 1.2

--- rectangle.cpp
+++ rectangle.cpp
@@ -1,23 +1,23 @@
 #include <iostream>
 #include <string>
 #include <math.h>
-using namespace std;
 
 int main(){
+
     int length;
     int width;
     int perimeter;
     int area;
     float diagonal;
 
-    cout << "Please enter the length and width of the rectangle: " << endl;
-    cin >> length;
-    cin >> width;
+    std::cout << "Please enter the length and width of the rectangle: " << std::endl;
+    std::cin >> length;
+    std::cin >> width;
 
     perimeter = length * 2 + width * 2;
     area = length * width;
     diagonal = sqrt(length * length + width * width);
 
-    cout << "The perimeter: " << perimeter << " area: " << area  << "," << " and diagonal " << diagonal;
+    std::cout << "The perimeter: " << perimeter << " area: " << area  << "," << " and diagonal " << diagonal;
 
 }
--- welcome.cpp
+++ welcome.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <string>
 using namespace std;
-int main(){
+
+int main() {
 
     string firstName;
     string lastName;
