29b0d9462f54878a62f456fabceee966f3c71dde
Eyup <eyupvroegop@gmail.com>
2019-09-09 16:28:14 +0200

    Finished assignment 2.4

--- quadratic.cpp
+++ quadratic.cpp
@@ -1,16 +1,31 @@
 #include <iostream>
+#include <math.h>
+#include <vector>
 using namespace std;
 
-int discriminantCalc(int a, int b, int c){
+double discriminantCalc(double a, double b, double c) {
     return (b * b) - (4*a*c);
+}
+
+vector<double> solutionCalc (double a, double b, double c, bool two) {
+    if (two) {
+        double positive = (-b + sqrt( (b*b) - (4*a*c) )) / (2 * a);
+        double negative = (-b - sqrt( (b*b) - (4*a*c) )) / (2 * a);
+        vector<double> sol = {positive, negative};
+        return sol;
+    } else {
+        double positive = (-b + sqrt( (b*b) - (4*a*c) )) / (2 * a);
+        vector<double> sol = {positive};
+        return sol;
+    }
 }
 
 int main() {
 
-    int a;
-    int b;
-    int c;
-    int discriminant;
+    double a;
+    double b;
+    double c;
+    double discriminant;
 
     cout << "Please enter the values of a, b, and c: ";
     cin >> a;
@@ -21,14 +36,12 @@ int main() {
 
     if (discriminant > 0) {
         cout << "There are 2 solutions." << endl;
-        cout << "The solutions are: <first_solution> and <second_solution>";
+        vector<double> solution =  solutionCalc(a, b ,c, true);
+        cout << "The solutions are: " << solution.at(0) << " and " << solution.at(1) << endl;
     } else if (discriminant == 0) {
         cout << "There is 1 solution." << endl;
-        cout << "The solution is: <solution>";
+        cout << "The solution is: " << solutionCalc(a, b, c, false).at(0) << endl;
     } else {
-        cout << "There is no solution.";
+        cout << "There is no solution." << endl;
     }
-
-
-    
 }
