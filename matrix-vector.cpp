d83c17af8c573650b6fa67bade115b2926fd3ba7
Eyup <eyupvroegop@gmail.com>
2019-09-16 22:24:14 +0200

    Finished assignment 3.2

---
+++ matrix-vector.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+
+    vector< vector<double> > table = {
+        {1.0, 2.0, 3.0},
+        {4.0, 5.0, 6.0},
+        {7.0, 8.0, 9.0}
+    };
+    vector<double> coef(3);
+    vector<double> result(3);
+
+    cout << "Please enter the three vector coefficients" << endl;
+    cin >> coef.at(0);
+    cin >> coef.at(1);
+    cin >> coef.at(2);
+
+    result.at(0) = table.at(0).at(0) * coef.at(0) + table.at(0).at(1) * coef.at(1) + table.at(0).at(2) * coef.at(2);
+    result.at(1) = table.at(1).at(0) * coef.at(0) + table.at(1).at(1) * coef.at(1) + table.at(1).at(2) * coef.at(2);
+    result.at(2) = table.at(2).at(0) * coef.at(0) + table.at(2).at(1) * coef.at(1) + table.at(2).at(2) * coef.at(2);
+
+    cout << "The result vector is [" << result.at(0) << ", " << result.at(1) << ", " << result.at(2) << "]";
+    
+}
