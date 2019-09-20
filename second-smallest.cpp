fa1d10db2065abcdf9aad3fa013828c60137bf2e
Eyup <eyupvroegop@gmail.com>
2019-09-20 14:49:20 +0200

    Finished assignment 3.3 with changes

--- second-smallest.cpp
+++ second-smallest.cpp
@@ -2,31 +2,30 @@
 #include <vector>
 using namespace std;
 
-int sortVector(vector<int> elements) {
+int findNumber(vector<int> elements) {
     
-    if(elements.size() < 2) {
+    if (elements.size() < 2) {
         throw runtime_error("error: no second smallest");
     }
 
-    int pointerSmaller = elements.at(0);
+    int pointerSmaller = INT32_MAX;
     int pointerSmallest = elements.at(0);
+    int hit = 0;
 
     for(int i = 0; i < elements.size(); i++){
-        
-        if(elements.at(i) < pointerSmallest) {
-            pointerSmaller = pointerSmallest;
+        if (elements.at(i) < pointerSmallest) {
             pointerSmallest = elements.at(i);
         }
-
-        if(elements.at(i) > pointerSmaller && pointerSmaller == pointerSmallest) {
-            pointerSmaller = elements.at(i);
-        } else if (elements.at(i) < pointerSmaller && pointerSmaller > pointerSmallest) {
-            pointerSmaller = elements.at(i);
-        }
-
     }
 
-    if(pointerSmallest == pointerSmaller) {
+    for(int i = 0; i < elements.size(); i++){
+        if (elements.at(i) <= pointerSmaller && pointerSmallest != elements.at(i)) {
+            pointerSmaller = elements.at(i);
+            hit++;
+        }
+    }
+
+    if (hit == 0) {
         throw runtime_error("error: no second smallest");
     }
 
@@ -49,18 +48,16 @@ int main() {
         if (cin.fail()) {
             break;
             con = false;
-
         } else {
             elements.push_back(number);
         }
     }
 
-    int result = sortVector(elements);
+    int result = findNumber(elements);
 
     cout << "The second smallest number is: " << result;
 
     } catch (runtime_error excpt) {
         cout << excpt.what() << endl;
     }
-
 }
